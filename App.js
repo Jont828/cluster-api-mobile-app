eef110aef823f25bf8859ff123512bd6973eb3cc
Jonathan Tong <jt572@cornell.edu>
2022-09-20 21:40:44 -0400

    Add some mock ups for cluster view

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { AppRegistry } from 'react-native';
+import { name as appName } from './app.json';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import App from './src/App';
 
-export default function App() {
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  version: 3,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#1976D2',
+    secondary: '#f1c40f',
+    tertiary: '#a1b2c3'
+  },
+};
+
+
+export default function Main() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider theme={theme}>
+      <App />
+    </PaperProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+AppRegistry.registerComponent(appName, () => Main);
---
+++ src/TargetCluster.js
@@ -0,0 +1,96 @@
+import * as React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Appbar, } from 'react-native-paper';
+import { Avatar, Button, Card } from 'react-native-paper';
+import { Caption, Headline, Paragraph, Subheading, Text, Title } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+
+const TargetCluster = () => (
+  <View>
+    <Appbar.Header elevated>
+      <Appbar.Action icon="menu" onPress={() => { }} />
+      {/* <Appbar.BackAction onPress={() => { }} /> */}
+      <Appbar.Content title="Clusters">
+      </Appbar.Content>
+      <Appbar.Action icon="dots-vertical" onPress={() => { }} />
+      {/* <Appbar.Action icon="magnify" onPress={() => { }} /> */}
+    </Appbar.Header>
+    <View style={styles.wrapper}>
+      <Title>my-namespace</Title>
+      <Card style={styles.card}>
+        {/* <Card.Title title="my-cluster" subtitle="Provisioned" /> */}
+        <Card.Content>
+          <Title>
+            my-cluster <Icon name="microsoft-azure" style={styles.provider}></Icon>
+          </Title>
+          <Caption>Provisioned</Caption>
+          <View style={styles.chevronWrap}>
+            <Icon name="chevron-right" style={styles.chevron}></Icon>
+          </View>
+        </Card.Content>
+      </Card>
+      <Card style={styles.card}>
+        {/* <Card.Title title="my-cluster" subtitle="Provisioned" /> */}
+        <Card.Content>
+          <Title>
+            my-other-cluster <Icon name="microsoft-azure" style={styles.provider}></Icon>
+          </Title>
+          <Caption>Provisioned</Caption>
+          <View style={styles.chevronWrap}>
+            <Icon name="chevron-right" style={styles.chevron}></Icon>
+          </View>
+        </Card.Content>
+      </Card>
+    </View>
+
+    <View style={styles.wrapper}>
+      <Title>my-other-namespace</Title>
+      <Card style={styles.card}>
+        {/* <Card.Title title="my-cluster" subtitle="Provisioned" /> */}
+        <Card.Content>
+          <Title>
+            <Icon name="microsoft-azure" style={styles.provider}></Icon>
+          </Title>
+          <Caption>Provisioned</Caption>
+          <View style={styles.chevronWrap}>
+            <Icon name="chevron-right" style={styles.chevron}></Icon>
+          </View>
+        </Card.Content>
+      </Card>
+    </View>
+  </View>
+);
+
+export default TargetCluster;
+
+const styles = StyleSheet.create({
+  wrapper: {
+    margin: 10,
+  },
+  card: {
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  chevronWrap: {
+    position: 'absolute',
+    right: 10,
+    top: '50%',
+    transform: [
+      { translateY: -5 } // TODO: don't hard code this
+    ]
+  },
+  chevron: {
+    fontSize: 40,
+    color: 'grey'
+  },
+  provider: {
+    marginLeft: 10,
+    fontSize: 20,
+    color: '#2196F3'
+  },
+  fab: {
+    position: 'absolute',
+    right: 16,
+  },
+});
