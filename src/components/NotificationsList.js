19d78aaffe393230bbea857a00eb7d87543d6473
Jonathan Tong <jt572@cornell.edu>
2022-10-12 19:03:26 -0400

    Set up swipeable list view for notifications

---
+++ src/components/NotificationsList.js
@@ -0,0 +1,196 @@
+import React, { useState } from 'react';
+import {
+  Animated,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TouchableHighlight,
+  View,
+} from 'react-native';
+
+import { SwipeListView } from 'react-native-swipe-list-view';
+import { Card, Subheading } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+const rowSwipeAnimatedValues = {};
+Array(20)
+  .fill('')
+  .forEach((_, i) => {
+    rowSwipeAnimatedValues[`${i}`] = new Animated.Value(0);
+  });
+
+export default function NotificationsList() {
+  const [listData, setListData] = useState(
+    Array(20)
+      .fill('')
+      .map((_, i) => ({ key: `${i}`, text: `item #${i}` }))
+  );
+
+  const closeRow = (rowMap, rowKey) => {
+    if (rowMap[rowKey]) {
+      rowMap[rowKey].closeRow();
+    }
+  };
+
+  const deleteRow = (rowMap, rowKey) => {
+    closeRow(rowMap, rowKey);
+    const newData = [...listData];
+    const prevIndex = listData.findIndex(item => item.key === rowKey);
+    newData.splice(prevIndex, 1);
+    setListData(newData);
+  };
+
+  const onRowDidOpen = rowKey => {
+    console.log('This row opened', rowKey);
+  };
+
+  const onSwipeValueChange = swipeData => {
+    const { key, value } = swipeData;
+    rowSwipeAnimatedValues[key].setValue(Math.abs(value));
+  };
+
+  const renderItem = data => (
+    <Card
+      onPress={() => console.log('You touched me')}
+      style={styles.rowFront}
+      // underlayColor={'#AAA'}
+    >
+      <Card.Title title={"List " + data.item.text}></Card.Title>
+      {/* <View>
+        <Text>I am {data.item.text} in a SwipeListView</Text>
+      </View> */}
+    </Card>
+  );
+
+  const renderHiddenItem = (data, rowMap) => (
+    <View style={styles.rowBack}>
+      <TouchableOpacity
+        style={[styles.backLeftBtn]}
+        onPress={() => closeRow(rowMap, data.item.key)}
+      >
+        <Text style={styles.backTextWhite}>Left</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[styles.backRightBtn, styles.backRightBtnLeft]}
+        onPress={() => closeRow(rowMap, data.item.key)}
+      >
+        <Text style={styles.backTextWhite}>Close</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.backRightBtn, styles.backRightBtnRight]}
+        onPress={() => deleteRow(rowMap, data.item.key)}
+      >
+        <Animated.View
+          style={[
+            styles.trash,
+            {
+              transform: [
+                {
+                  scale: rowSwipeAnimatedValues[
+                    data.item.key
+                  ].interpolate({
+                    inputRange: [45, 90],
+                    outputRange: [0, 1],
+                    extrapolate: 'clamp',
+                  }),
+                },
+              ],
+            },
+          ]}
+        >
+          <Icon name="delete" size={24} color="white"></Icon>
+          {/* <Image
+            source={require('../images/trash.png')}
+            style={styles.trash}
+          /> */}
+        </Animated.View>
+      </TouchableOpacity>
+    </View>
+  );
+
+  return (
+    <View style={styles.container}>
+      <SwipeListView
+        data={listData}
+        renderItem={renderItem}
+        renderHiddenItem={renderHiddenItem}
+        leftOpenValue={75}
+        rightOpenValue={-150}
+        previewRowKey={'0'}
+        previewOpenValue={-40}
+        previewOpenDelay={3000}
+        onRowDidOpen={onRowDidOpen}
+        onSwipeValueChange={onSwipeValueChange}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'transparent',
+    flex: 1,
+  },
+  backTextWhite: {
+    color: '#FFF',
+  },
+  rowFront: {
+    // alignItems: 'center',
+    // backgroundColor: '#CCC',
+    // borderBottomColor: 'black',
+    // borderBottomWidth: 1,
+    // justifyContent: 'center',
+    height: 50,
+    marginBottom: 10,
+    // marginVertical: 5,
+  },
+  rowBack: {
+    alignItems: 'center',
+    backgroundColor: '#DDD',
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingLeft: 15,
+    height: 50,
+    marginBottom: 10,
+    borderRadius: 3,
+    // borderRadius: 10,
+    // marginVertical: 5,
+
+  },
+  backLeftBtn: {
+    alignItems: 'center',
+    bottom: 0,
+    justifyContent: 'center',
+    position: 'absolute',
+    top: 0,
+    width: 75,
+    borderTopLeftRadius: 3,
+    borderBottomLeftRadius: 3,
+    backgroundColor: '#1976D2',
+  },
+  backRightBtn: {
+    alignItems: 'center',
+    bottom: 0,
+    justifyContent: 'center',
+    position: 'absolute',
+    top: 0,
+    width: 75,
+  },
+  backRightBtnLeft: {
+    backgroundColor: '#1976D2',
+    right: 75,
+  },
+  backRightBtnRight: {
+    borderTopRightRadius: 3,
+    borderBottomRightRadius: 3,
+    backgroundColor: '#ff5252',
+    right: 0,
+  },
+  trash: {
+    height: 25,
+    width: 25,
+  },
+});
--- src/screens/Notifications.js
+++ src/screens/Notifications.js
@@ -1,12 +1,44 @@
 import React from "react";
-import { ScrollView, Text } from 'react-native';
+import { ScrollView, Text, StyleSheet, View } from 'react-native';
+import { Card } from 'react-native-paper';
+import NotificationsList from '../components/NotificationsList';
+import { SwipeListView } from 'react-native-swipe-list-view';
 
-const Notifications = () => {
+const Notifications = ({ route, navigation }) => {
+
+  const aboutItems = {
+    "Version": "0.1.0-alpha",
+    "Authors": ["Jonathan Tong", "William Yao"]
+  };
+
+  const settingsItems = {
+    "Theme": "Light",
+    "Notifications": ["TODO"]
+  };
+  let listViewData = Array(20)
+    .fill("")
+    .map((_, i) => ({ key: `${i}`, text: `item #${i}` }));
+
   return (
-    <ScrollView>
-      <Text>Notifications</Text>
+    <ScrollView style={styles.wrapper}>
+      <NotificationsList></NotificationsList>
     </ScrollView>
   );
 }
 
 export default Notifications;
+
+const styles = StyleSheet.create({
+  wrapper: {
+    paddingHorizontal: 10,
+    marginVertical: 15,
+  },
+  rowFront: {
+    height: 50,
+    marginVertical: 10,
+  },
+  rowBack: {
+    height: 50,
+    marginVertical: 10,
+  }
+});
